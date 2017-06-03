033a409c848b61b6886d6ca616807cf64a84356e
PATRA <ap2270@att.com>
2017-06-03 17:33:52 -0400

    Implemented a simple calculator program

--- calculator.py
+++ calculator.py
@@ -3,3 +3,37 @@ Created on Jun 3, 2017
 
 @author: AP2270
 '''
+
+def add(num1, num2):
+    return (num1 + num2)
+
+def subtract(num1, num2):
+    return (num1 - num2)
+
+def multiply(num1, num2):
+    return (num1 * num2)
+
+def divide(num1, num2):
+    return (num1 / num2)
+
+def main():
+    operation = input('Enter a operation to be performed')
+    if (operation != '+' and operation !='-' and operation !='*' and operation != "/"):
+        print('Invalid Operation Entered. Please Enter one of +,-,* or /')
+    else:
+        num1 = int(input('Enter a number '))
+        num2 = int(input('Enter the second number'))
+        if(operation == '+'):
+            return add(num1, num2)
+        elif(operation == '-'):
+            return subtract(num1, num2)
+        elif(operation == '*'):
+            return multiply(num1, num2)
+        elif(operation == '/'):
+            return divide(num1, num2)
+        else:
+            print('Nothing to be done')
+    
+print('Welcome to the Calculator')  
+result = main()
+print('The result of the operation is ', result)

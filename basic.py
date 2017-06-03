acc81cb336c6e91df686d79f51e82b425619d728
PATRA <ap2270@att.com>
2017-06-03 11:31:45 -0400

    Added basic.py and datatypes.py

---
+++ basic.py
@@ -0,0 +1,33 @@
+'''
+Created on Jun 3, 2017
+
+@author: AP2270
+'''
+
+# This is a comment in python
+
+"""
+This is a multi line 
+comment for python
+"""
+
+'''
+This is another multiline comment 
+for python
+'''
+
+print('Hello World') # printing in single quote
+print("Hello World") # printing in double quote
+
+print(''' This is how a paragraph
+is printed in python''') # Prints a paragraph
+
+print('Hello'[0]) # prints the element @ index 0
+print('Hello'[0:3]) # prints the elements from 0 to index 3
+
+print(1, 2,3,4,'Hello') # prints everything in single line
+
+print('one\ntwo\n') # prints in new line
+
+
+
---
+++ datatypes.py
@@ -0,0 +1,33 @@
+'''
+Created on Jun 3, 2017
+
+@author: AP2270
+'''
+# number integer
+from test.pickletester import MyTuple
+num = 12345
+print(type(num), num)
+
+#string type
+var = 'This is a variable'
+print(type(var), var)
+
+#float type
+varD = 1.5
+print(type(varD), varD)
+
+
+#list type of data
+myList = [1, 2,3,4,5]
+print(type(myList), myList)
+
+#dictionary data type
+myDictionary = {'a':1, 'b':2, 'c':3}
+print(type(myDictionary), myDictionary)
+
+#tuple 
+myTuple = (1,2,3,4,5)
+print(type(myTuple), myTuple)
+
+
+
